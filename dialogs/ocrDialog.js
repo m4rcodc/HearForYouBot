205e420314f3cbb0d943ac7f0ac0264f687d3856
Marco <marcodellecave1998@gmail.com>
2022-01-02 19:31:18 +0100

    OcrDialog

--- dialogs/ocrDialog.js
+++ dialogs/ocrDialog.js
@@ -1,11 +1,25 @@
+'use strict';
 
-
-//import { QnACardBuilder } from "botbuilder-ai";
-//import { Context } from "microsoft-cognitiveservices-speech-sdk/distrib/lib/src/common.speech/RecognizerConfig";
-
+const async = require('async');
 const fs = require('fs');
-
+const https = require('https');
 const path = require("path");
+const createReadStream = require('fs').createReadStream
+const sleep = require('util').promisify(setTimeout);
+const ComputerVisionClient = require('@azure/cognitiveservices-computervision').ComputerVisionClient;
+const ApiKeyCredentials = require('@azure/ms-rest-js').ApiKeyCredentials;
+/**
+ * AUTHENTICATE
+ * This single client is used for all examples.
+ */
+const key = 'f949748f29f546fd8199180f2d052826';
+const endpoint = 'https://ocrhearforyou.cognitiveservices.azure.com/';
+
+const computerVisionClient = new ComputerVisionClient(
+    new ApiKeyCredentials({ inHeader: { 'Ocp-Apim-Subscription-Key': key } }), endpoint);
+
+const axios = require('axios').default;
+const { v4: uuidv4 } = require('uuid');
 /**
  * AUTHENTICATE
  * This single client is used for all examples.
@@ -29,11 +43,11 @@ const {
     Attachment
 } = require('botbuilder-dialogs');
 
-const { AttachmentClass }= require('./attachment.js');
 
 const WATERFALL_DIALOG = 'WATERFALL_DIALOG';
 const OCR_DIALOG = 'OCR_DIALOG';
 const ATT_PROMPT = 'ATT_PROMPT';
+var value = null;
 
 class OcrDialog extends ComponentDialog {
     constructor(userState) {
@@ -44,6 +58,7 @@ class OcrDialog extends ComponentDialog {
         this.addDialog(new AttachmentPrompt('ATT_PROMPT'));
         this.addDialog(new WaterfallDialog(WATERFALL_DIALOG, [
             this.introStep.bind(this),
+            this.downloadAttachStep.bind(this),
             this.ocrStep.bind(this)
         ]));
 
@@ -69,42 +84,85 @@ class OcrDialog extends ComponentDialog {
 
     }
 
-    async ocrStep(step) {
-        console.log(step.results);
-        console.log("sono qui");
-        const att = new AttachmentClass();
-        att.downloadAttachmentAndWrite(step);
-    }
+    async downloadAttachStep(step) {
 
-}
-
-/*
-async function handleIncomingAttachment(turnContext) {
-    // Prepare Promises to download each attachment and then execute each Promise.
-    const promises = turnContext.activity.attachments.map(this.downloadAttachmentAndWrite);
-    const successfulSaves = await Promise.all(promises);
-
-    // Replies back to the user with information about where the attachment is stored on the bot's server,
-    // and what the name of the saved file is.
-    async function replyForReceivedAttachments(localAttachmentData) {
-        if (localAttachmentData) {
-            // Because the TurnContext was bound to this function, the bot can call
-            // `TurnContext.sendActivity` via `this.sendActivity`;
-            await this.sendActivity(`Attachment "${ localAttachmentData.fileName }" ` +
-                `has been received and saved to "${ localAttachmentData.localPath}".`);
-        } else {
-            await this.sendActivity('Attachment was not successfully saved to disk.');
+        const result = step.result; 
+        const attach = Object.values(result);
+        console.log(attach);
+        for (const key in attach) {
+            if (attach.hasOwnProperty(key)) {
+                value = attach[key];
+                console.log(value.name);
+        
+            }
         }
-    }
-
-    // Prepare Promises to reply to the user with information about saved attachments.
-    // The current TurnContext is bound so `replyForReceivedAttachments` can also send replies.
-    const replyPromises = successfulSaves.map(replyForReceivedAttachments.bind(turnContext));
-    await Promise.all(replyPromises);
+        downloadAttachmentAndWrite(value);
 }
 
+    async ocrStep(step){
 
-async function downloadAttachmentAndWrite(attachment) {
+
+    }
+}
+    async function computerVision() {
+        async.series([
+          async function () {
+                // Status strings returned from Read API. NOTE: CASING IS SIGNIFICANT.
+                // Before Read 3.0, these are "Succeeded" and "Failed"
+                const STATUS_SUCCEEDED = "succeeded";
+                const STATUS_FAILED = "failed"
+
+                 const localImagePath = __dirname + value.name ;//qui andrà il value.name dell'attachment
+
+                 console.log('\Reading local image for text in ...', path.basename(localImagePath));
+
+
+                 console.log('\nReadwritten text from local file...', localImagePath);
+                const writingResult = await readTextFromFile(computerVisionClient, localImagePath);
+                printRecText(writingResult);
+
+                async function readTextFromFile(client, localImagePath) {
+                    // To recognize text in a local image, replace client.read() with readTextInStream() as shown:
+                    let result = await client.readInStream(() => createReadStream(localImagePath));
+                    // Operation ID is last path segment of operationLocation (a URL)
+                    let operation = result.operationLocation.split('/').slice(-1)[0];
+            
+                    // Wait for read recognition to complete
+                    // result.status is initially undefined, since it's the result of read
+                    while (result.status !== STATUS_SUCCEEDED) { await sleep(1000); result = await client.getReadResult(operation); }
+                    return result.analyzeResult.readResults; // Return the first page of result. Replace [0] with the desired page if this is a multi-page file such as .pdf or .tiff.
+                  }
+
+                  function printRecText(readResults) {
+                    console.log('Recognized text:');
+                    for (const page in readResults) {
+                      if (readResults.length > 1) {
+                        console.log(`==== Page: ${page}`);
+                      }
+                      const result = readResults[page];
+                      if (result.lines.length) {
+                        for (const line of result.lines) {
+                          console.log(line.words.map(w => w.text).join(' '));
+                        }
+                      }
+                      else { console.log('No recognized text.'); }
+                    }
+                  }
+                 
+
+        }, 
+        function () {
+            return new Promise((resolve) => {
+              resolve();
+            })
+          }
+        ], (err) => {
+          throw (err);
+        });
+    } 
+
+ async function downloadAttachmentAndWrite(attachment) {
+    console.log("sono qui");
     // Retrieve the attachment via the attachment's contentUrl.
     const url = attachment.contentUrl;
 
@@ -135,7 +193,8 @@ async function downloadAttachmentAndWrite(attachment) {
         fileName: attachment.name,
         localPath: localFileName
     };
+    
+    }
 
-    */
 module.exports.OcrDialog =OcrDialog;
 module.exports.OCR_DIALOG = this.OCR_DIALOG;
